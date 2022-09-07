77983a65044ee4ae2f8cff9cc253bd6a56cc06de
Richard Ramos <info@richardramos.me>
2022-09-07 15:15:58 -0400

    chore: add iOS libraries (#2)

--- scripts/build.sh
+++ scripts/build.sh
@@ -38,26 +38,23 @@ cross build --release --lib --target=arm-unknown-linux-gnueabihf
 cross build --release --lib --target=armv7-linux-androideabi
 cross build --release --lib --target=i686-pc-windows-gnu
 cross build --release --lib --target=i686-unknown-linux-gnu
-cross build --release --lib --target=mips-unknown-linux-gnu
-cross build --release --lib --target=mips64-unknown-linux-gnuabi64
-cross build --release --lib --target=mips64el-unknown-linux-gnuabi64
-cross build --release --lib --target=mipsel-unknown-linux-gnu
 cross build --release --lib --target=x86_64-linux-android
 cross build --release --lib --target=x86_64-pc-windows-gnu
 cross build --release --lib --target=x86_64-unknown-linux-gnu
 cross build --release --lib --target=x86_64-unknown-linux-musl
 
 # TODO: these work only on iOS
-#rustup target add aarch64-apple-ios x86_64-apple-ios
-#cross build --release --target=x86_64-apple-darwin --lib
-#cross build --release --target=aarch64-apple-darwin --lib
-#cargo lipo --release --targets=aarch64-apple-ios,x86_64-apple-ios
-
+rustup target add aarch64-apple-ios x86_64-apple-ios x86_64-apple-darwin aarch64-apple-darwin
+cargo build --release --target=x86_64-apple-darwin --lib
+cargo build --release --target=aarch64-apple-darwin --lib
+cargo build --release --target=x86_64-apple-ios --lib
+cargo build --release --target=aarch64-apple-ios --lib
+#cargo lipo --release
 popd
 
 TOOLS_DIR=`dirname $0`
 COMPILE_DIR=${TOOLS_DIR}/../zerokit/target
-#rm -rf $COMPILE_DIR/x86_64-apple-ios $COMPILE_DIR/aarch64-apple-ios
+rm -rf $COMPILE_DIR/x86_64-apple-ios $COMPILE_DIR/aarch64-apple-ios
 for platform in `ls ${COMPILE_DIR} | grep -v release | grep -v debug`
 do
   PLATFORM_DIR=${DIRECTORY}/$platform
